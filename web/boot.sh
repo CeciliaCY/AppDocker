a17d8b6b01fc04ca8fb54c4f966f153397582b7b
Chengying He <30835953+CeciliaCY@users.noreply.github.com>
2019-11-17 17:35:27 -0500

    Remove comments

--- web/boot.sh
+++ web/boot.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 set -e
 eval $(python3 exporter.py)
-# export FLASK_2FA='12345678901'
-# export FLASK_PASSWORD='Administrator@1'
-# export FLASK_SECRET='jfldjlfjoifejrlejfldjfldjflsa'
 python3 app.py
